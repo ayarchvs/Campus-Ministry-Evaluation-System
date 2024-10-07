2ee1bc9fb33dcda22d91846601ddfb1eaec073bf
Mikee <mikoy2004i@gmail.com>
2024-10-07 21:56:10 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$dbHost = 'localhost';  
+$dbUsername = 'root';   
+$dbPassword = '';       
+$dbName = 'campus_ministry'; 
+
+
+$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
