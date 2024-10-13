94bee96581ff60d9469b13b4723580517ae33e39
Mikee <mikoy2004i@gmail.com>
2024-10-13 18:19:49 +0800

    Update campus_ministry.sql

--- campus_ministry.sql
+++ campus_ministry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2024 at 03:37 PM
+-- Generation Time: Oct 13, 2024 at 12:19 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -24,32 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `accounts`
+-- Table structure for table `event`
 --
 
-CREATE TABLE `accounts` (
-  `Account_ID` int(11) NOT NULL,
-  `Name` varchar(100) NOT NULL,
-  `Role` enum('Admin','Retreat','Recollection1','Recollection2') NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Email` varchar(100) NOT NULL
+CREATE TABLE `event` (
+  `Event_ID` int(11) NOT NULL,
+  `Staff_ID` int(11) DEFAULT NULL,
+  `E_Name` varchar(100) NOT NULL,
+  `E_Year` int(11) NOT NULL,
+  `E_Month` int(11) NOT NULL,
+  `E_Day` int(11) NOT NULL,
+  `E_Religion` varchar(50) NOT NULL,
+  `E_Location` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `events`
+-- Table structure for table `staff`
 --
 
-CREATE TABLE `events` (
-  `Event_ID` int(11) NOT NULL,
-  `Account_ID` int(11) DEFAULT NULL,
-  `Year` int(11) NOT NULL,
-  `Month` int(11) NOT NULL,
-  `Day` int(11) NOT NULL,
-  `Religion` varchar(50) NOT NULL,
-  `Location` varchar(100) NOT NULL,
-  `FileName` text NOT NULL
+CREATE TABLE `staff` (
+  `Staff_ID` int(11) NOT NULL,
+  `S_Name` varchar(100) NOT NULL,
+  `S_Type` varchar(50) NOT NULL,
+  `S_Password` varchar(255) NOT NULL,
+  `S_Email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -57,44 +57,43 @@ CREATE TABLE `events` (
 --
 
 --
--- Indexes for table `accounts`
+-- Indexes for table `event`
 --
-ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`Account_ID`),
-  ADD UNIQUE KEY `Email` (`Email`);
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`Event_ID`),
+  ADD KEY `Staff_ID` (`Staff_ID`);
 
 --
--- Indexes for table `events`
+-- Indexes for table `staff`
 --
-ALTER TABLE `events`
-  ADD PRIMARY KEY (`Event_ID`),
-  ADD KEY `Account_ID` (`Account_ID`);
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`Staff_ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `accounts`
+-- AUTO_INCREMENT for table `event`
 --
-ALTER TABLE `accounts`
-  MODIFY `Account_ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `event`
+  MODIFY `Event_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `events`
+-- AUTO_INCREMENT for table `staff`
 --
-ALTER TABLE `events`
-  MODIFY `Event_ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `staff`
+  MODIFY `Staff_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `events`
+-- Constraints for table `event`
 --
-ALTER TABLE `events`
-  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`Account_ID`) REFERENCES `accounts` (`Account_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `event`
+  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`Staff_ID`) REFERENCES `staff` (`Staff_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
