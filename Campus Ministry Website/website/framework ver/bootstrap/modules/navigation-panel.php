285f6b60d71049bd8b74c9bd55ade21657c59d33
Kuroe911 <151163378+Kuroe911@users.noreply.github.com>
2024-11-10 21:38:27 +0800

    Updated: Page flow (owki for now)
    1. login flow good
    
    2. old new event data goog
    
    Need:
    1. make eventlist for the actual flow of adding and viewing to event detail

--- Campus Ministry Website/website/framework ver/bootstrap/modules/navigation-panel.php
+++ Campus Ministry Website/website/framework ver/bootstrap/modules/navigation-panel.php
@@ -10,7 +10,7 @@
             
             <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
                 <!-- Navbar Brand-->
-                <a class="navbar-brand ps-3" href="index.php">Start Bootstrap</a>
+                <a class="navbar-brand ps-3" href="main-page.php">Start Bootstrap</a>
                 <!-- Sidebar Toggle-->
                 <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
                 <!-- Navbar Search-->
@@ -39,7 +39,7 @@
                         <div class="sb-sidenav-menu">
                             <div class="nav">
                                 <div class="sb-sidenav-menu-heading">Core</div>
-                                <a class="nav-link" href="index.php">
+                                <a class="nav-link" href="main-page.php">
                                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                     Dashboard
                                 </a>
@@ -68,7 +68,7 @@
                                         </a>
                                         <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
                                             <nav class="sb-sidenav-menu-nested nav">
-                                                <a class="nav-link" href="login.html">Login</a>
+                                                <a class="nav-link" href="index.html">Login</a>
                                                 <a class="nav-link" href="register.html">Register</a>
                                                 <a class="nav-link" href="password.html">Forgot Password</a>
                                             </nav>
