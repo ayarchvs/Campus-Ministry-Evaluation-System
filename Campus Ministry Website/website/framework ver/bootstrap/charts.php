23f7fb17e1578c892267031ef870028576174710
DinsLim <dinsdalelim360@gmail.com>
2024-11-14 18:33:06 +0800

    Update charts.php

--- Campus Ministry Website/website/framework ver/bootstrap/charts.php
+++ Campus Ministry Website/website/framework ver/bootstrap/charts.php
@@ -28,7 +28,7 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Retreat Evaluation Charts</h1>
+                        <h1 class="mt-4">Retreat Evaluation Chart</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
                             <li class="breadcrumb-item active">Charts</li>
