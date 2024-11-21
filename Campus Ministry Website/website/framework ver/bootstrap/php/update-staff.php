30423ed13fbf449a47299f6fee2343308dac44f9
Kachuu-GitGud <machew.kachu@gmail.com>
2024-11-21 23:23:03 +0800

    added fail safe if there is no updated password

--- Campus Ministry Website/website/framework ver/bootstrap/php/update-staff.php
+++ Campus Ministry Website/website/framework ver/bootstrap/php/update-staff.php
@@ -3,25 +3,30 @@
 
 include "../config/config.php";  // Include DB connection
 
-if (isset($_POST['staffId'], $_POST['staffFirstName'], $_POST['staffLastName'], $_POST['staffType'], $_POST['staffEmail'], $_POST['staffPassword'])) {
-    
+if (isset($_POST['staffId'], $_POST['staffFirstName'], $_POST['staffLastName'], $_POST['staffType'], $_POST['staffEmail'])) {    
     // Retrieve posted data
     $staffID = $_POST['staffId'];
     $staffFirstName = $_POST['staffFirstName'];
     $staffLastName = $_POST['staffLastName'];
     $staffType = $_POST['staffType'];
     $staffEmail = $_POST['staffEmail'];
-    $staffPassword = $_POST['staffPassword'];
 
     $Name = $staffLastName . ", " . $staffFirstName;  // Concatenate names
 
-    
-
-    // Hash the password using bcrypt
-    $hashedPassword = password_hash($staffPassword, PASSWORD_DEFAULT);
-
     // SQL query to update staff data
-    $query = "UPDATE staff SET S_Name = '$Name', S_Type = '$staffType', S_Password = '$hashedPassword', S_Email = '$staffEmail' WHERE Staff_ID = '$staffID'";
+    $query = "UPDATE staff SET S_Name = '$Name', S_Type = '$staffType', S_Email = '$staffEmail' WHERE Staff_ID = '$staffID'";
+
+    // if not empty staffPassword, then update query to include password
+    if (isset($_POST['staffPassword']) && !empty($_POST['staffPassword'])) {
+        $staffPassword = $_POST['staffPassword'];
+
+        // Hash the password using bcrypt
+        $hashedPassword = password_hash($staffPassword, PASSWORD_DEFAULT);
+
+        // Add password update to the query
+        $query = "UPDATE staff SET S_Name = '$Name', S_Type = '$staffType', S_Password = '$hashedPassword', S_Email = '$staffEmail' WHERE Staff_ID = '$staffID'";
+    }
+
     $result = $conn->query($query);
 
     if (!$result) {
